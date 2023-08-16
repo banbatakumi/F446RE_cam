85e5d0befbe97942636a9e61b9d468f3beb7fd4b
banbatakumi <bantech0210@gmail.com>
2023-08-16 15:49:40 +0900

    UART追加

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,55 @@
 #include "mbed.h"
 
+// UART通信定義 (TX, RX)
+Serial main_mcu(PB_6, PB_7);
+Serial cam_1(PA_2, PA_3);
+Serial cam_2(PC_12, PD_2);
+Serial cam_3(PA_9, PA_10);
+Serial cam_4(PC_6, PC_7);
+
+// 関数定義
+void main_mcu_rx();
+void cam_1_rx();
+void cam_2_rx();
+void cam_3_rx();
+void cam_4_rx();
+
 // ピン定義
-DigitalOut led_2(PA_6);
+DigitalOut led_1(PB_10);
+DigitalOut led_2(PB_2);
+DigitalOut led_3(PB_1);
+DigitalOut led_4(PB_0);
 
 // グローバル変数定義
 
 int main() {
+      // 通信速度: 9600, 14400, 19200, 28800, 38400, 57600, 115200
+      main_mcu.baud(38400);
+      main_mcu.attach(main_mcu_rx, Serial::RxIrq);
+      cam_1.baud(38400);
+      cam_1.attach(cam_1_rx, Serial::RxIrq);
+      cam_2.baud(38400);
+      cam_2.attach(cam_2_rx, Serial::RxIrq);
+      cam_3.baud(38400);
+      cam_3.attach(cam_3_rx, Serial::RxIrq);
+      cam_4.baud(38400);
+      cam_4.attach(cam_4_rx, Serial::RxIrq);
+
       while (1) {
       }
+}
+
+void main_mcu_rx() {
+}
+
+void cam_1_rx() {
+}
+
+void cam_2_rx() {
+}
+
+void cam_3_rx() {
+}
+
+void cam_4_rx() {
 }
