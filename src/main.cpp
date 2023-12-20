f0c59013633312ea6f38e5fb5a641d846aca0a19
banbatakumi <bantech0210@gmail.com>
2023-12-20 23:22:04 +0900

    移動平均削除

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,12 @@
 #include "m1n.h"
 #include "mbed.h"
-#include "moving_ave.h"
 #include "simplify_deg.h"
 
 #define CAM_QTY 4
 
-#define SendTime(baud_, byte_num_) 1 / baud_ * 10 * byte_num_ * 1000000
-
 // UART通信定義 (TX, RX)
 RawSerial mainSerial(PC_10, PC_11);
 M1n m1n[CAM_QTY] = {M1n(PA_2, PA_3), M1n(PC_12, PD_2), M1n(PA_9, PA_10), M1n(PC_6, PC_7)};
-
-MovingAve ballDirAve;
-MovingAve ballDisAve;
-MovingAve yGoalDirAve;
-MovingAve yGoalSizeAve;
-MovingAve bGoalDirAve;
-MovingAve bGoalSizeAve;
 
 // 関数定義
 void MainMcu();
@@ -47,13 +37,6 @@ void setup() {
       // 通信速度: 9600, 14400, 19200, 28800, 38400, 57600, 115200
       mainSerial.baud(230400);
 
-      ballDirAve.SetLength(5);
-      ballDisAve.SetLength(5);
-      yGoalDirAve.SetLength(15);
-      yGoalSizeAve.SetLength(15);
-      bGoalDirAve.SetLength(15);
-      bGoalSizeAve.SetLength(15);
-
       __disable_irq();   // 禁止
       for (int i = 0; i < 4; i++) {
             led[i] = 1;
@@ -80,14 +63,6 @@ int main() {
             BallConversion();
             YGoalConversion();
             BGoalConversion();
-
-            // それぞれの値に移動平均をかける
-            ballDirAve.Compute(&rslt_ball_dir);
-            ballDisAve.Compute(&rslt_ball_dis);
-            yGoalDirAve.Compute(&rslt_yellow_goal_dir);
-            yGoalSizeAve.Compute(&rslt_yellow_goal_size);
-            bGoalDirAve.Compute(&rslt_blue_goal_dir);
-            bGoalSizeAve.Compute(&rslt_blue_goal_size);
 
             MainMcu();
       }
@@ -187,5 +162,4 @@ void MainMcu() {
       for (uint8_t i = 0; i < send_byte_num; i++) {
             mainSerial.putc(send_byte[i]);
       }
-      wait_us(SendTime(230400, send_byte_num));
 }
